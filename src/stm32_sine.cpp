6ceaebeba03f8d8b6063ac41b96c1be4030048e3
Modellfan <febaschu@aol.com>
2026-04-28 21:43:28 +0200

    Spread mapped CAN transmit slots

--- src/stm32_sine.cpp
+++ src/stm32_sine.cpp
@@ -60,6 +60,7 @@ static CanMap* canMap;
 static CanSdo* canSdo;
 static Terminal* terminal;
 static bool seenBrakePedal = false;
+static uint8_t canMapTxSlot = 0;
 
 static void Ms100Task(void)
 {
@@ -101,8 +102,6 @@ static void Ms100Task(void)
    Param::SetFloat(Param::uac, uac);
    #endif // CONTROL
 
-   if (Param::GetInt(Param::canperiod) == CAN_PERIOD_100MS)
-      canMap->SendAll();
 }
 
 static void RunCharger(float udc)
@@ -253,8 +252,20 @@ static void Ms10Task(void)
 
    Param::SetInt(Param::uptime, rtc_get_counter_val());
 
+   if (Param::GetInt(Param::canperiod) == CAN_PERIOD_100MS)
+   {
+      canMap->SendByIndex(canMapTxSlot);
+      canMapTxSlot = (canMapTxSlot + 1) % MAX_MESSAGES;
+   }
+}
+
+static void Ms1Task(void)
+{
    if (Param::GetInt(Param::canperiod) == CAN_PERIOD_10MS)
-      canMap->SendAll();
+   {
+      canMap->SendByIndex(canMapTxSlot);
+      canMapTxSlot = (canMapTxSlot + 1) % MAX_MESSAGES;
+   }
 }
 
 /** This function is called when the user changes a parameter */
@@ -295,8 +306,8 @@ void Param::Change(Param::PARAM_NUM paramNum)
       case Param::throtmin:
       case Param::idcmin:
       case Param::idcmax:
-      case Param::offthrotregen:
-         //These are candidates to be frequently set by CAN, so we handle them separately
+      case Param::offthprotregen:
+        //These are candidates to be frequently set by CAN, so we handle them separately
          Throttle::throtmax = Param::GetFloat(Param::throtmax);
          Throttle::throtmin = Param::GetFloat(Param::throtmin);
          Throttle::idcmin = Param::GetFloat(Param::idcmin);
@@ -315,7 +326,7 @@ void Param::Change(Param::PARAM_NUM paramNum)
          PwmGeneration::SetControllerGains(Param::GetInt(Param::iqkp),
                                            Param::GetInt(Param::idkp),
                                            Param::GetInt(Param::exckp),
-                                           Param::GetInt(Param::curki));
+                                           Param::GetInt(Param::curkp));
          Encoder::SwapSinCos((Param::GetInt(Param::pinswap) & SWAP_RESOLVER) > 0);
          FOC::SetMotorParameters(Param::GetFloat(Param::lqminusld) / 1000.0f, Param::GetFloat(Param::fluxlinkage) / 1000.0f);
          FOC::SetMaximumModulationIndex(Param::GetInt(Param::modmax));
@@ -456,6 +467,7 @@ extern "C" int main(void)
 
    s.AddTask(Ms100Task, 100);
    s.AddTask(Ms10Task, 10);
+   s.AddTask(Ms1Task, 1);
 
    DigIo::prec_out.Set();
 
@@ -498,4 +510,3 @@ extern "C" int main(void)
 
    return 0;
 }
-
